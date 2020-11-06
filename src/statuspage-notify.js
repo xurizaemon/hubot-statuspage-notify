f7048d2cbcdd5dada8506bbe63c13a95c76f7c01
Chris Burgess <chrisburgess@catalyst.net.nz>
2020-11-07 07:25:06 +1300

    👁 Monitor StatusCloud and report changes in status

--- src/statuspage-notify.js
+++ src/statuspage-notify.js
@@ -7,30 +7,73 @@
 //   HUBOT_STATUSPAGE_NOTIFY_CHANNELS - Rooms to announce status events in.
 //   HUBOT_STATUSPAGE_NOTIFY_IGNORE - @TODO Regex of keys / values to ignore
 //
-// Commands:
-//   hubot cloud status
 
 module.exports = function(robot) {
-    let request = require('http')
-
     let required = [
         'HUBOT_STATUSPAGE_NOTIFY_PAGES',
-        'HUBOT_STATUSPAGE_NOTIFY_PAGES'
+        'HUBOT_STATUSPAGE_NOTIFY_CHANNELS'
     ]
-    required.forEach((var) => {
-        if (typeof process.env[var] === 'undefined') {
-            robot.logger.warn(`hubot-statuspage-notify requires ${var}`)
-            return
+
+    let pluginName = 'statuscloud-notify'
+    let monitorPages = process.env.HUBOT_STATUSPAGE_NOTIFY_PAGES || ''
+    let notifyChannels = process.env.HUBOT_STATUSPAGE_NOTIFY_CHANNELS || ''
+    let checkInterval = process.env.HUBOT_STATUSPAGE_NOTIFY_INTERVAL || 3
+    let timer
+
+    // Seconds to ms.
+    checkInterval = checkInterval * 1000
+
+    /**
+     * Validate configuration and set up timers.
+     */
+    setupTimers = () => {
+        let validConfiguration = true
+
+        required.forEach((varName) => {
+            if (typeof process.env[varName] === 'undefined') {
+                robot.logger.warning(`hubot-statuspage-notify requires configuring ${varName}`)
+                validConfiguration = false  
+            }
+        })
+
+        if (validConfiguration) {
+            robot.logger.debug('Setting up timers')
+            monitorPages = monitorPages.split(' ')
+            notifyChannels = notifyChannels.split(' ')
+
+            robot.logger.debug(`Monitoring statuscloud for ${monitorPages.join(', ')} and will notify rooms ${notifyChannels.join(', ')}`)
+            robot.brain.set(`${pluginName}.monitorPages`, monitorPages)
+            robot.brain.set(`${pluginName}.notifyChannels`, notifyChannels)
+            timer = setInterval(checkStatusPage, checkInterval)
         }
-    })
 
-    let pages = process.env.HUBOT_STATUSPAGE_NOTIFY_PAGES || ''
-    let notifyRooms = process.env.HUBOT_STATUSPAGE_NOTIFY_CHANNELS || ''
-
-    if (pages === '' || channels === '') {
-        robot.logger.warn('hubot-statuspage-notify configuration requires: , HUBOT_STATUSPAGE_NOTIFY_PAGES')
     }
-    robot.logger.debug(`HUBOT_STATUSPAGE_NOTIFY_PAGES = ${HUBOT_STATUSPAGE_NOTIFY_PAGES}`)
-    robot.logger.debug(`HUBOT_STATUSPAGE_NOTIFY_CHANNELS = ${HUBOT_STATUSPAGE_NOTIFY_CHANNELS}`)
-  
-  }  
+
+    checkStatusPage = () => {
+        pages = robot.brain.get(`${pluginName}.monitorPages`)
+        pages.forEach((page) => {
+            // robot.logger.debug(`StatusCloud check: ${page}.`)
+            let pageUrl = `https://${page}.statuspage.io/api/v2/status.json`
+            let key = `${pluginName}.results.${page}`
+            let title = page
+            robot.http(pageUrl).get()((err, res, body) => {
+                if (err) throw `HTTP exception at ${pageUrl}`
+                if (res.statusCode !== 200) {
+                    robot.logger.warning(`HTTP ${res.statusCode} response from ${url}`)
+                    throw `HTTP exception at ${pageUrl}`
+                }
+
+                let prevResult = robot.brain.get(`${key}.status`) || { status: { description: 'No result yet' } }
+                let curResult = JSON.parse(body)
+
+                if (typeof curResult.page.name !== 'undefined') title = curResult.page.name
+                if (curResult.status.description !== prevResult.status.description) {
+                    robot.messageRoom('shell', `StatusPage for ${title} has changed to ${curResult.status.description}`)
+                }
+                robot.brain.set(`${key}.status`, curResult)
+            })
+        })
+    }
+
+    setupTimers()
+}
