c690e53f129c5953b3811feb516de8b5523600c5
Chris Burgess <chrisburgess@catalyst.net.nz>
2020-11-08 21:09:31 +1300

    👂 hubot statuspage monitor <slug> command

--- src/statuspage-notify.js
+++ src/statuspage-notify.js
@@ -1,11 +1,16 @@
 // Description:
 //   StatusPage notifications for Hubot
 //
+// Watches the configured StatusPage sites for updates and announces them in channel.
+//
 // Configuration:
 //   HUBOT_STATUSPAGE_NOTIFY_PAGES - StatusCloud page slugs (eg 'catalystcloud' or 'linode') to monitor.
 //   HUBOT_STATUSPAGE_NOTIFY_INTERVAL - Seconds between checks
 //   HUBOT_STATUSPAGE_NOTIFY_CHANNELS - Rooms to announce status events in.
 //
+// Usage:
+//   hubot statuspage monitor <slug> - Start watching for updates from https://<slug>.statuscloud.io
+//   hubot statuspage don't monitor <slub> - Stop watching for updates from https://<slug>.statuscloud.io
 
 module.exports = (robot) => {
   const required = [
@@ -41,7 +46,38 @@ module.exports = (robot) => {
       robot.brain.set(`${pluginName}.notifyChannels`, notifyChannels)
       setInterval(checkStatusPage, checkInterval)
     }
-  }
+
+    robot.respond(/statuspage monitor ([a-zA-Z0-9-_]+)/i, (res) => {
+        let page = res.match[1].toLowerCase()
+        robot.logger.debug(`Request to start monitoring ${page}`)
+        let monitorPages = robot.brain.get(`${pluginName}.monitorPages`)
+        var index = monitorPages.indexOf(page);
+        if (index === -1) {
+          monitorPages.push(page)
+          robot.brain.set(`${pluginName}.monitorPages`, monitorPages)
+          robot.logger.debug(`Monitoring: ${monitorPages.join(', ')}.`)
+          res.reply(`Now monitoring: ${monitorPages.join(', ')}.`)
+        }
+        else {
+          res.reply(`Already monitoring ${page}.`)
+        }
+      })
+
+      robot.respond(/statuspage don't monitor ([a-zA-Z0-9-_]+)/i, (res) => {
+        let page = res.match[1].toLowerCase()
+        robot.logger.debug(`Request to stop monitoring ${page}`)
+        let monitorPages = robot.brain.get(`${pluginName}.monitorPages`)
+        var index = monitorPages.indexOf(page);
+        if (index !== -1) {
+          monitorPages.splice(index, 1);
+          robot.brain.set(`${pluginName}.monitorPages`, monitorPages)
+          res.reply(`Now monitoring: ${monitorPages.join(', ')}.`)
+        }
+        else {
+          res.reply(`Was not monitoring ${page}.`)
+        }
+      })
+    }
 
   const checkStatusPage = () => {
     const pages = robot.brain.get(`${pluginName}.monitorPages`)
@@ -62,7 +98,6 @@ module.exports = (robot) => {
 
         const prevResult = robot.brain.get(`${key}.status`) || { page: { updated_at: '' } }
         const curResult = JSON.parse(body)
-        // console.log(curResult, 'curResult status')
 
         if (typeof curResult.page.name !== 'undefined') title = curResult.page.name
         if (curResult.page.updated_at > prevResult.page.updated_at) {
