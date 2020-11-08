bf2e1319b2de3a65191b768af4b5e7d0cbfa95c4
Chris Burgess <chrisburgess@catalyst.net.nz>
2020-11-08 16:20:56 +1300

    💄 StandardJS style

--- src/statuspage-notify.js
+++ src/statuspage-notify.js
@@ -7,111 +7,107 @@
 //   HUBOT_STATUSPAGE_NOTIFY_CHANNELS - Rooms to announce status events in.
 //
 
-module.exports = function(robot) {
-    let required = [
-        'HUBOT_STATUSPAGE_NOTIFY_PAGES',
-        'HUBOT_STATUSPAGE_NOTIFY_CHANNELS'
-    ]
+module.exports = (robot) {
+  const required = [
+    'HUBOT_STATUSPAGE_NOTIFY_PAGES',
+    'HUBOT_STATUSPAGE_NOTIFY_CHANNELS'
+  ]
 
-    let pluginName = 'statuscloud-notify'
-    let monitorPages = process.env.HUBOT_STATUSPAGE_NOTIFY_PAGES || ''
-    let notifyChannels = process.env.HUBOT_STATUSPAGE_NOTIFY_CHANNELS || ''
-    let checkInterval = process.env.HUBOT_STATUSPAGE_NOTIFY_INTERVAL || 3
-    let timer
+  const pluginName = 'statuscloud-notify'
+  let monitorPages = process.env.HUBOT_STATUSPAGE_NOTIFY_PAGES || ''
+  let notifyChannels = process.env.HUBOT_STATUSPAGE_NOTIFY_CHANNELS || ''
+  let checkInterval = process.env.HUBOT_STATUSPAGE_NOTIFY_INTERVAL || 3
 
-    // Seconds to ms.
-    checkInterval = checkInterval * 1000
+  // Seconds to ms.
+  checkInterval = checkInterval * 1000
 
-    /**
-     * Validate configuration and set up timers.
-     */
-    setupTimers = () => {
-        let validConfiguration = true
+  const init = () => {
+    let validConfiguration = true
 
-        required.forEach((varName) => {
-            if (typeof process.env[varName] === 'undefined') {
-                robot.logger.warning(`hubot-statuspage-notify requires configuring ${varName}`)
-                validConfiguration = false  
-            }
-        })
+    required.forEach((varName) => {
+      if (typeof process.env[varName] === 'undefined') {
+        robot.logger.warning(`hubot-statuspage-notify requires configuring ${varName}`)
+        validConfiguration = false
+      }
+    })
 
-        if (validConfiguration) {
-            robot.logger.debug('Setting up timers')
-            monitorPages = monitorPages.split(' ')
-            notifyChannels = notifyChannels.split(' ')
+    if (validConfiguration) {
+      robot.logger.debug('Setting up timers')
+      monitorPages = monitorPages.split(' ')
+      notifyChannels = notifyChannels.split(' ')
 
-            robot.logger.debug(`Monitoring statuscloud for ${monitorPages.join(', ')} and will notify rooms ${notifyChannels.join(', ')}`)
-            robot.brain.set(`${pluginName}.monitorPages`, monitorPages)
-            robot.brain.set(`${pluginName}.notifyChannels`, notifyChannels)
-            timer = setInterval(checkStatusPage, checkInterval)
+      robot.logger.debug(`Monitoring statuscloud for ${monitorPages.join(', ')} and will notify rooms ${notifyChannels.join(', ')}`)
+      robot.brain.set(`${pluginName}.monitorPages`, monitorPages)
+      robot.brain.set(`${pluginName}.notifyChannels`, notifyChannels)
+      setInterval(checkStatusPage, checkInterval)
+    }
+  }
+
+  const checkStatusPage = () => {
+    const pages = robot.brain.get(`${pluginName}.monitorPages`)
+    pages.forEach((page) => {
+      // robot.logger.debug(`StatusCloud check: ${page}.`)
+      const pageUrl = `https://${page}.statuspage.io/api/v2/status.json`
+      const incidentsUrl = `https://${page}.statuspage.io/api/v2/incidents/unresolved.json`
+      const key = `${pluginName}.results.${page}`
+      let title = page
+
+      // Check and report status.
+      robot.http(pageUrl).get()((err, res, body) => {
+        if (err) throw new Error(`HTTP exception at ${pageUrl}`)
+        if (res.statusCode !== 200) {
+          robot.logger.warning(`HTTP ${res.statusCode} response from ${pageUrl}`)
+          throw new Error(`HTTP ${res.statusCode} at ${pageUrl}`)
         }
 
-    }
+        const prevResult = robot.brain.get(`${key}.status`) || { page: { updated_at: '' } }
+        const curResult = JSON.parse(body)
+        // console.log(curResult, 'curResult status')
 
-    checkStatusPage = () => {
-        pages = robot.brain.get(`${pluginName}.monitorPages`)
-        pages.forEach((page) => {
-            // robot.logger.debug(`StatusCloud check: ${page}.`)
-            let pageUrl = `https://${page}.statuspage.io/api/v2/status.json`
-            let incidentsUrl = `https://${page}.statuspage.io/api/v2/incidents/unresolved.json`
-            let key = `${pluginName}.results.${page}`
-            let title = page
+        if (typeof curResult.page.name !== 'undefined') title = curResult.page.name
+        if (curResult.page.updated_at > prevResult.page.updated_at) {
+          const message = `${title} status is ${curResult.status.description} at ${curResult.page.updated_at}`
+          notifyChannels.forEach((channel) => {
+            robot.messageRoom(channel, message)
+          })
+        }
+        robot.brain.set(`${key}.status`, curResult)
+      })
 
-            // Check and report status.
-            robot.http(pageUrl).get()((err, res, body) => {
-                if (err) throw `HTTP exception at ${pageUrl}`
-                if (res.statusCode !== 200) {
-                    robot.logger.warning(`HTTP ${res.statusCode} response from ${url}`)
-                    throw `HTTP exception at ${pageUrl}`
-                }
+      // Check and report incidents.
+      robot.http(incidentsUrl).get()((err, res, body) => {
+        if (err) throw new Error(`HTTP exception at ${incidentsUrl}`)
+        if (res.statusCode !== 200) {
+          robot.logger.warning(`HTTP ${res.statusCode} response from ${incidentsUrl}`)
+          throw new Error(`HTTP ${res.statusCode} at ${incidentsUrl}`)
+        }
 
-                let prevResult = robot.brain.get(`${key}.status`) || { page: { updated_at: '' } }
-                let curResult = JSON.parse(body)
-                // console.log(curResult, 'curResult status')
+        const prevResult = robot.brain.get(`${key}.incident`) || { updated_at: '' }
+        const curResult = JSON.parse(body)
 
-                if (typeof curResult.page.name !== 'undefined') title = curResult.page.name
-                if (curResult.page.updated_at > prevResult.page.updated_at) {
-                    let message = `${title} status is ${curResult.status.description} at ${curResult.page.updated_at}`
-                    notifyChannels.forEach((channel) => {
-                        robot.messageRoom(channel, message)
-                    })
-                }
-                robot.brain.set(`${key}.status`, curResult)
+        curResult.incidents.forEach((incident) => {
+          if (incident.updated_at > prevResult.updated_at) {
+            const affectedComponents = incident.components.reduce((incidents, component) => {
+              incidents.push(`- ${component.name} (${component.status})`)
+              return incidents
+            }, []).join('\n')
+
+            const message = [
+              `${title} ${incident.impact} incident: ${incident.name}`,
+              `${affectedComponents}`,
+              `- ${incident.shortlink}`,
+              `${incident.incident_updates[0].body}`
+            ].join('\n')
+
+            notifyChannels.forEach((channel) => {
+              robot.messageRoom(channel, message)
             })
-
-            // Check and report incidents.
-            robot.http(incidentsUrl).get()((err, res, body) => {
-                if (err) throw `HTTP exception at ${incidentsUrl}`
-                if (res.statusCode !== 200) {
-                    robot.logger.warning(`HTTP ${res.statusCode} response from ${url}`)
-                }
-
-                let prevResult = robot.brain.get(`${key}.incident`) || { updated_at: '' }
-                curResult = JSON.parse(body)
-
-                curResult.incidents.forEach((incident) => {
-                    if (incident.updated_at > prevResult.updated_at) {
-                        let affectedComponents = incident.components.reduce((incidents, component) => {
-                            incidents.push(`- ${component.name} (${component.status})`)
-                            return incidents
-                        }, []).join('\n')
-
-                        let message = [
-                            `${title} ${incident.impact} incident: ${incident.name}`,
-                            `${affectedComponents}`,
-                            `- ${incident.shortlink}`,
-                            `${incident.incident_updates[0].body}`
-                        ].join('\n')
-
-                        notifyChannels.forEach((channel) => {
-                            robot.messageRoom(channel, message)
-                        })
-                        robot.brain.set(`${key}.incident`, incident)
-                    }
-                })
-            })
+            robot.brain.set(`${key}.incident`, incident)
+          }
         })
-    }
+      })
+    })
+  }
 
-    setupTimers()
+  init()
 }
